e1af8504f96028a620b23a559487c56fa485a20b
vishwas-adhikari <vishwas.s.1002@gmail.com>
2025-07-25 15:25:11 +0530

    FIX: Correct DJANGO_SETTINGS_MODULE in wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,6 +1,10 @@
+# backend/config/wsgi.py
+
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+# --- THIS IS THE FIX ---
+# Change 'backend.settings' to 'config.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
