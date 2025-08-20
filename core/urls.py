2f4d86c4fe89760c01e768788ac833ac642a5a71
vishwas-adhikari <vishwas.s.1002@gmail.com>
2025-08-20 17:06:43 +0530

    Revert "Fix: Cron job"
    This reverts commit 2cc4c9cbce70ac66b964946ad000a7c0ed90fdae.

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BlogPostViewSet, ProjectViewSet, AboutInfoViewSet, health_check
+from .views import BlogPostViewSet, ProjectViewSet, AboutInfoViewSet
 
 router = DefaultRouter()
 router.register(r'blog-posts', BlogPostViewSet, basename='blogpost')
@@ -9,5 +9,4 @@ router.register(r'about', AboutInfoViewSet, basename='about')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('health-check/', health_check, name='health-check'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,6 @@
 from rest_framework import viewsets
 from .models import BlogPost, Project, AboutInfo
 from .serializers import BlogPostSerializer, ProjectSerializer, AboutInfoSerializer
-from django.http import JsonResponse #health check 
 
 class BlogPostViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = BlogPost.objects.all().order_by('-publication_date')
@@ -15,11 +14,3 @@ class ProjectViewSet(viewsets.ReadOnlyModelViewSet):
 class AboutInfoViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = AboutInfo.objects.all()
     serializer_class = AboutInfoSerializer
-
-
-def health_check(request):
-    """
-    A simple view that returns a 200 OK response with minimal data.
-    Perfect for a pinger service.
-    """
-    return JsonResponse({"status": "ok"})
